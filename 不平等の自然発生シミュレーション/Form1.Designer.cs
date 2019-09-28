13cf280763bcc00b1b2bf79da89a25b26d8fccd7
TomokoFukui <tomoko.fukui@f-softhouse.com>
2019-09-29 00:58:59 +0900

    Add files via upload

---
+++ 不平等の自然発生シミュレーション/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace 不平等の自然発生シミュレーション
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.buttonStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(21, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(752, 393);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(706, 415);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button buttonStart;
+    }
+}
+
---
+++ 不平等の自然発生シミュレーション/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+/// <summary>
+/// 平等な初期状態からランダムに行動した結果、偏りが生じた状態で安定するという事象のシミュレーション。
+/// 最初の所持金は全員等しく１万円。ランダムに決めた相手に1000円渡す。お金がないときは渡せない。
+/// </summary>
+namespace 不平等の自然発生シミュレーション
+{
+    public partial class Form1 : Form
+    {
+        readonly int population = 100000;           // 人口
+        readonly int money = 10000;                 // 所持金
+        int[] people;                               // 人々の所持金リスト
+        Random rand = new Random();
+
+        readonly string chart_area1 = "Area1";
+        readonly string legend1 = "Graph1";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chart1.ChartAreas.Clear();
+            chart1.ChartAreas.Add(new ChartArea(chart_area1));
+            chart1.ChartAreas[0].AxisX.Title = "所持金(%)";
+            chart1.ChartAreas[0].AxisX.IsLabelAutoFit = false;
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            chart1.ChartAreas[0].AxisY.Title = "人数比(%)";
+            chart1.ChartAreas[0].AxisY.IsLabelAutoFit = false;
+            chart1.ChartAreas[0].AxisY.Minimum = 0;
+
+            people = new int[population];
+            for (int i = 0; i < people.Length; i++)
+            {
+                people[i] = money;
+            }
+            DrawChart();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            while (true)
+            {
+                RunGame();
+                DrawChart();
+                Thread.Sleep(100);
+            }
+        }
+
+        private void RunGame()
+        {
+            for (int i = 0; i < people.Length; i++)
+            {
+                if (people[i] > 0)
+                {
+                    int selected = rand.Next(population);
+                    if (selected == i)
+                    {
+                        selected = rand.Next(population);
+                    }
+                    people[selected] += 1000;
+                    people[i] -= 1000;
+                }
+            }
+        }
+
+        private void DrawChart()
+        {
+            var table = new Dictionary<int, int>();
+            for (int i = 0; i < people.Length; i++)
+            {
+                int key = people[i];
+
+                if (table.ContainsKey(key) == true)
+                {
+                    int count = table[key] + 1;
+                    table[key] = count;
+                }
+                else
+                {
+                    table.Add(key, 1);
+                }
+            }
+
+            chart1.Series.Clear();
+            chart1.Series.Add(legend1);
+            chart1.Series[0].ChartType = SeriesChartType.Point;
+            chart1.Series[0].IsVisibleInLegend = false;
+            chart1.Series[0].IsValueShownAsLabel = false;
+
+            foreach (var item in table)
+            {
+                double percentPopulation = (double)item.Value * 100.0 / (double)population;
+                double percentMoney = (double)item.Key * 100.0 / (double)money;
+                DataPoint dp = new DataPoint(percentMoney, percentPopulation);
+                chart1.Series[0].Points.Add(dp);
+            }
+            chart1.Update();
+        }
+    }
+}
